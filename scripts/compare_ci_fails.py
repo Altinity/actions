4ba779c9d8c303c94311e976cb414db2489a27b0
strtgbb <146047128+strtgbb@users.noreply.github.com>
2025-06-05 18:57:21 -0400

    compare fails: rework inputs for greater flexibility

--- scripts/compare_ci_fails.py
+++ scripts/compare_ci_fails.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import re
+import requests
 
 from clickhouse_driver import Client
 from datetime import timedelta
@@ -10,10 +12,41 @@ DATABASE_USER_VAR = "CHECKS_DATABASE_USER"
 DATABASE_PASSWORD_VAR = "CHECKS_DATABASE_PASSWORD"
 
 
-def get_checks_fails(client: Client, job_url: str, include_broken=False):
+def get_tag_commit(clickhouse_tag, repo="ClickHouse/ClickHouse"):
+    # Get the commit associated with the tag
+    tag_api_url = f"https://api.github.com/repos/{repo}/git/refs/tags/{clickhouse_tag}"
+    tag_response = requests.get(tag_api_url)
+    tag_details = tag_response.json()
+
+    if "object" not in tag_details:
+        print(f"Error: Tag {clickhouse_tag} not found")
+        return
+
+    if tag_details["object"]["type"] == "tag":
+        # If it's an annotated tag, get the commit it points to
+        tag_object_url = tag_details["object"]["url"]
+        tag_object_response = requests.get(tag_object_url)
+        tag_object_details = tag_object_response.json()
+        return tag_object_details["object"]["sha"]
+    else:
+        # If it's a lightweight tag, it points directly to the commit
+        return tag_details["object"]["sha"]
+
+
+def get_checks_fails(
+    client: Client, job_url=None, commit_sha=None, include_broken=False
+):
     """
-    Get tests that did not succeed for the given job URL.
+    Get tests that did not succeed for the given job URL or commit.
     """
+    assert not (job_url and commit_sha)
+    if job_url:
+        where_clause = f"task_url='{job_url}'"
+    elif commit_sha:
+        where_clause = f"commit_sha='{commit_sha}'"
+    else:
+        raise ValueError("Either job_url or commit_sha must be provided")
+
     # , check_start_time as start_time
     columns = "splitByString(' [', check_name)[1] as check_group, splitByString(' [', check_name)[2] as group_id, "
     columns += "test_name, check_status, test_status, report_url as link"
@@ -21,9 +54,19 @@ def get_checks_fails(client: Client, job_url: str, include_broken=False):
     if include_broken:
         statuses += ", 'BROKEN'"
 
-    query = f"""SELECT {columns} FROM `gh-data`.checks
-                WHERE task_url='{job_url}'
-                AND (test_status IN ({statuses})
+    query = f"""SELECT {columns}
+                FROM (
+                    SELECT
+                    check_name,
+                    test_name,
+                    argMax(check_status, check_start_time) as check_status,
+                    argMax(test_status, check_start_time) as test_status,
+                    argMax(report_url, check_start_time) as report_url
+                    FROM `gh-data`.checks
+                    WHERE {where_clause}
+                    GROUP BY check_name, test_name
+                )
+                WHERE (test_status IN ({statuses})
                 OR check_status=='error')
                 ORDER BY test_name, check_group
                 """
@@ -32,26 +75,52 @@ def get_checks_fails(client: Client, job_url: str, include_broken=False):
     return statuses
 
 
-def get_checks_statuses(client: Client, job_url: str, checks_fails):
+def get_checks_statuses(client: Client, checks_fails, job_url=None, commit_sha=None):
     """
-    Get statuses of all checks for the given job URL.
+    Get statuses of all checks for the given job URL or commit.
     """
+    assert not (job_url and commit_sha), "Either job_url or commit_sha must be provided"
+    if job_url:
+        where_clause = f"task_url='{job_url}'"
+    elif commit_sha:
+        where_clause = f"commit_sha='{commit_sha}'"
+    else:
+        raise ValueError("Either job_url or commit_sha must be provided")
+
     tests = tuple(
         (row["check_group"], row["test_name"]) for _, row in checks_fails.iterrows()
     )
 
-    columns = "splitByString(' [', check_name)[1] as check_group, "
-    columns += "test_name, check_status, test_status, report_url as link"
-    query = f"""SELECT {columns} FROM `gh-data`.checks
-                WHERE task_url='{job_url}'
+    query = f"""SELECT 
+                  splitByString(' [', check_name)[1] as check_group, 
+                  test_name,
+                  argMax(check_status, check_start_time) as check_status,
+                  argMax(test_status, check_start_time) as test_status,
+                  argMax(report_url, check_start_time) as link
+                FROM `gh-data`.checks
+                WHERE {where_clause}
                 AND (check_group, test_name) IN {tests}
+                GROUP BY check_group, test_name
                 ORDER BY test_name, check_group
                 """
+    statuses = client.query_dataframe(query)
+    return statuses
 
-    return client.query_dataframe(query)
 
+def get_upstream_statuses(checks_fails, commit_sha=None, clickhouse_version=None):
+    """
+    Get statuses of all checks for the given commit or version.
+    """
+    assert not (
+        clickhouse_version and commit_sha
+    ), "Either clickhouse_version or commit_sha must be provided"
+    if clickhouse_version:
+        where_clause = f"head_ref='{clickhouse_version}'"
+    elif commit_sha:
+        where_clause = f"commit_sha='{commit_sha}'"
+    else:
+        raise ValueError("Either clickhouse_version or commit must be provided")
 
-def get_upstream_statuses(checks_fails, clickhouse_version):
     tests = tuple(
         (row["check_group"], row["test_name"])
         for _, row in checks_fails.iterrows()
@@ -62,9 +131,7 @@ def get_upstream_statuses(checks_fails, clickhouse_version):
     )
     print("Will check status of", len(tests), "upstream tests")
     assert len(tests) > 0
-    # print('Tests:', tests)
-    # return
-    # --max(check_start_time) as start_time
+
     query = f"""SELECT 
                   splitByString(' [', check_name)[1] as check_group, 
                   test_name,
@@ -73,7 +140,7 @@ def get_upstream_statuses(checks_fails, clickhouse_version):
                   argMax(report_url, check_start_time) as link,
                   max(check_start_time) as start_time
                 FROM default.checks
-                WHERE head_ref='{clickhouse_version}'
+                WHERE {where_clause}
                 AND (check_group, test_name) IN {tests}
                 GROUP BY check_group, test_name
                 ORDER BY test_name, check_group
@@ -101,15 +168,32 @@ def get_upstream_statuses(checks_fails, clickhouse_version):
     return upstream_statuses
 
 
+def merge_statuses(checks_fails_1, checks_fails_2, suffixes=("_1", "_2")):
+    combined_df = (
+        checks_fails_1.merge(
+            checks_fails_2,
+            on=["check_group", "test_name"],
+            suffixes=suffixes,
+            how="left",
+        )
+        .astype(str)
+        .replace("nan", "N/A")
+    )
+
+    return combined_df
+
+
 def compare_to_upstream(
     db_client, actions_run_url, clickhouse_version, include_broken=False
 ):
 
-    checks_fails = get_checks_fails(
+    checks_fails = get_checks_fails_for_job(
         db_client, actions_run_url, include_broken=include_broken
     )
 
-    upstream_statuses = get_upstream_statuses(checks_fails, clickhouse_version)
+    upstream_statuses = get_upstream_statuses_for_version(
+        checks_fails, clickhouse_version
+    )
 
     combined_df = checks_fails.merge(
         upstream_statuses,
@@ -132,11 +216,13 @@ def compare_two_runs(
     db_client, actions_run_url_1, actions_run_url_2, include_broken=False
 ):
 
-    checks_fails_1 = get_checks_fails(
+    checks_fails_1 = get_checks_fails_for_job(
         db_client, actions_run_url_1, include_broken=include_broken
     )
 
-    checks_fails_2 = get_checks_statuses(db_client, actions_run_url_2, checks_fails_1)
+    checks_fails_2 = get_checks_statuses_for_job(
+        db_client, actions_run_url_2, checks_fails_1
+    )
     print(len(checks_fails_1), len(checks_fails_2))
 
     combined_df = (
@@ -153,10 +239,12 @@ def compare_two_runs(
     return combined_df
 
 
-def print_results_md(results):
+def print_results_md(results, drop_columns=None):
+    if drop_columns is None:
+        drop_columns = []
     print(
         results.drop(
-            columns=["link_1", "link_2", "link_upstream", "link_altinity"],
+            columns=drop_columns,
             errors="ignore",
         ).to_markdown(index=False)
     )
@@ -193,15 +281,17 @@ def export_results_md(results, args, filename):
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Create a combined CI report.")
     parser.add_argument(
-        "--actions-run-url", required=True, help="URL of the actions run"
+        "--current-ref",
+        required=True,
+        help="Reference to make comparisons against. Ref can be a workflow url, commit hash, or git tag.",
     )
     parser.add_argument(
-        "--clickhouse-version",
-        help="MAJOR.MINOR version of upstream ClickHouse, e.g. 24.12. Exclusive with --actions-run-url-2",
+        "--previous-ref",
+        help="Reference to compare with. Ref can be a workflow url, commit hash, or git tag.",
     )
     parser.add_argument(
-        "--actions-run-url-2",
-        help="URL of a second actions run to compare against. Exclusive with --clickhouse-version",
+        "--upstream-ref",
+        help="Reference to compare with. Ref can be a MAJOR.MINOR version, commit hash or git tag.",
     )
     parser.add_argument(
         "--broken",
@@ -224,32 +314,92 @@ def main():
         settings={"use_numpy": True},
     )
 
-    if args.clickhouse_version and args.actions_run_url_2:
-        print("Error: --clickhouse-version and --actions-run-url-2 are exclusive")
+    if not (args.upstream_ref or args.previous_ref):
+        print("Error: Either --upstream-ref or --previous-ref must be provided")
         exit(1)
 
-    if args.clickhouse_version:
-        results = compare_to_upstream(
-            db_client, args.actions_run_url, args.clickhouse_version, args.broken
-        )
-        filename = f"compared_fails_{args.clickhouse_version}_{args.actions_run_url.split('/')[-1]}"
-
-    elif args.actions_run_url_2:
-        results = compare_two_runs(
+    if (
+        args.current_ref.startswith("https://github.com/")
+        and "/actions/runs/" in args.current_ref
+    ):
+        current_failures = get_checks_fails(db_client, job_url=args.current_ref)
+    elif args.current_ref.startswith("v"):
+        current_failures = get_checks_fails(
             db_client,
-            args.actions_run_url,
-            args.actions_run_url_2,
-            args.broken,
+            commit_sha=get_tag_commit(args.current_ref, repo="Altinity/ClickHouse"),
         )
-        filename = f"compared_fails_{args.actions_run_url.split('/')[-1]}_{args.actions_run_url_2.split('/')[-1]}"
-
+    elif len(args.current_ref) == 40:
+        current_failures = get_checks_fails(db_client, commit_sha=args.current_ref)
     else:
-        print("Error: --clickhouse-version or --actions-run-url-2 is required")
+        print("Error: --current-ref must be a workflow url, commit hash, or git tag")
         exit(1)
 
-    print_results_md(results)
-    export_results_csv(results, filename)
-    export_results_md(results, args, filename)
+    previous_failures = None
+    if args.previous_ref:
+        if (
+            args.previous_ref.startswith("https://github.com/")
+            and "/actions/runs/" in args.previous_ref
+        ):
+            previous_failures = get_checks_statuses(
+                db_client, current_failures, job_url=args.previous_ref
+            )
+        elif args.previous_ref.startswith("v"):
+            previous_failures = get_checks_statuses(
+                db_client,
+                current_failures,
+                commit_sha=get_tag_commit(
+                    args.previous_ref, repo="Altinity/ClickHouse"
+                ),
+            )
+        elif len(args.previous_ref) == 40:
+            previous_failures = get_checks_statuses(
+                db_client, current_failures, commit_sha=args.previous_ref
+            )
+        else:
+            print(
+                "Error: --previous-ref must be a workflow url, commit hash, or git tag"
+            )
+            exit(1)
+
+    upstream_failures = None
+    if args.upstream_ref:
+        if re.match(r"\d+\.\d+", args.upstream_ref):
+            upstream_failures = get_upstream_statuses(
+                current_failures, clickhouse_version=args.upstream_ref
+            )
+        elif args.upstream_ref.startswith("v"):
+            upstream_failures = get_upstream_statuses(
+                current_failures,
+                commit_sha=get_tag_commit(
+                    args.upstream_ref, repo="ClickHouse/ClickHouse"
+                ),
+            )
+        elif len(args.upstream_ref) == 40:
+            upstream_failures = get_upstream_statuses(
+                current_failures, commit_sha=args.upstream_ref
+            )
+        else:
+            print(
+                "Error: --upstream-ref must be a MAJOR.MINOR version, commit hash or git tag"
+            )
+            exit(1)
+
+    if args.previous_ref:
+        print("\nComparing with previous version")
+        results = merge_statuses(
+            current_failures, previous_failures, suffixes=("_current", "_previous")
+        )
+        print_results_md(results, drop_columns=["link_current", "link_previous"])
+
+    if args.upstream_ref:
+        print("\nComparing with upstream version")
+        results = merge_statuses(
+            current_failures, upstream_failures, suffixes=("_current", "_upstream")
+        )
+        print_results_md(results, drop_columns=["link_current", "link_upstream"])
+
+    # export_results_csv(results, filename)
+    # export_results_md(results, args, filename)
 
 
 if __name__ == "__main__":
